d0b4212d00efedc70eb10f73a3960d44451b56d1
Anupal Mishra <anupal.pm@gmail.com>
2024-04-13 20:11:32 +0100

    increase token expiry

--- api/utils.py
+++ api/utils.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = "aDapTive@123secret"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 999999
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
